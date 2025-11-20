7a763ed626cb56c39520d8da439e97580cf1b946
Mshu72 <m.shu.720@gmail.com>
2025-11-20 22:55:22 +0900

    Add Sieve of Eratosthenes implementation
    Implement the Sieve of Eratosthenes algorithm to find prime numbers up to a given limit.

---
+++ Algorithm/PrimeNumber/eratosthenes.py
@@ -0,0 +1,15 @@
+import math
+
+def get_prime(n):
+  if n <= 1:
+    retun []
+  prime = [2]
+  limit = int(math.sqrt(n))
+
+  # 奇数リストの作成
+  data = [i + 1 for i in range(2, n, 2)]
+  while limit > data[0]:
+    prime.append(data[0])
+    # 割り切れない数だけを取り出す
+    data = [j for j in data if j % data[0] !=0]
+  return prime + data
